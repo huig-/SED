854e3ad71b75c170c391921cd446bfb854ba4043
huig- <igago@ucm.es>
2016-04-11 19:17:45 +0200

    fix

--- practica3/Debug/8led.d
+++
@@ -1,7 +0,0 @@
-8led.d 8led.o: ../8led.c \
- C:\hlocal\EclipseARMwspace\prac3\commonEclipse/44b.h \
- C:\hlocal\EclipseARMwspace\prac3\commonEclipse/option.h
-
-C:\hlocal\EclipseARMwspace\prac3\commonEclipse/44b.h:
-
-C:\hlocal\EclipseARMwspace\prac3\commonEclipse/option.h:
