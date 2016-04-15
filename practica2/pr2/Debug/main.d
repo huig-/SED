fee3f3f5c586287af1b26c96a2b9a391f8f2e82c
huig- <igago@ucm.es>
2016-04-15 17:16:44 +0200

    Arreglar repositorio

---
+++ practica2/pr2/Debug/main.d
@@ -0,0 +1,10 @@
+main.d main.o: ../main.c \
+ C:\hlocal\EclipseARMwspace\pr2\commonEclipse/44blib.h \
+ C:\hlocal\EclipseARMwspace\pr2\commonEclipse/44b.h \
+ C:\hlocal\EclipseARMwspace\pr2\commonEclipse/option.h
+
+C:\hlocal\EclipseARMwspace\pr2\commonEclipse/44blib.h:
+
+C:\hlocal\EclipseARMwspace\pr2\commonEclipse/44b.h:
+
+C:\hlocal\EclipseARMwspace\pr2\commonEclipse/option.h:
---
+++ practica2/pr2/Debug/timer.d
@@ -0,0 +1,10 @@
+timer.d timer.o: ../timer.c \
+ C:\hlocal\EclipseARMwspace\pr2\commonEclipse/44b.h \
+ C:\hlocal\EclipseARMwspace\pr2\commonEclipse/option.h \
+ C:\hlocal\EclipseARMwspace\pr2\commonEclipse/44blib.h
+
+C:\hlocal\EclipseARMwspace\pr2\commonEclipse/44b.h:
+
+C:\hlocal\EclipseARMwspace\pr2\commonEclipse/option.h:
+
+C:\hlocal\EclipseARMwspace\pr2\commonEclipse/44blib.h:
